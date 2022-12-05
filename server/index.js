d2a7825d43d91959de64861046fe3f22e7ab02dd
Furkan <gami.furkan@gmail.com>
2022-12-05 15:14:25 +0300

    Project Completed

--- server/index.js
+++ server/index.js
@@ -3,22 +3,22 @@ import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
 
-import postRouters from "./routes/posts.js";
+import postRoutes from "./routes/posts.js";
 
 const app = express();
 
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
-app.use("/posts", postRouters);
 
-const CONNECTION_URL = "mongodb+srv://<string>:<string>@cluster0.iov9vuf.mongodb.net/?retryWrites=true&w=majority";
+app.use("/posts", postRoutes);
 
+const CONNECTION_URL = "mongodb+srv://<String>:<String>@practice.jto9p.mongodb.net/test";
 const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Server Running on port: ${PORT}`)))
-  .catch((error) => console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
 
 mongoose.set("useFindAndModify", false);
